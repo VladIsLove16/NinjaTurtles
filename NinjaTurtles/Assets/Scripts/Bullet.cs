255b2db9541e6543d8c6fa4c4e3505f8d2161737
Fluk <aleshagavr@mail.ru>
2023-11-13 01:56:50 +0300

    Пока закомментирован bullet и добавлены сущности, которые наглядно показывают работу скриптов

--- NinjaTurtles/Assets/Scripts/Bullet.cs
+++ NinjaTurtles/Assets/Scripts/Bullet.cs
@@ -1,25 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Bullet : MonoBehaviour 
-{
+// public class Bullet : MonoBehaviour 
+// {
 	
-	//public float speed = 20f;
-	public int damage = 40;
-	//public Rigidbody2D rb;
+// 	//public float speed = 20f;
+// 	public int damage = 40;
+// 	//public Rigidbody2D rb;
 
-	//void Start(){
-		//rb.velocity = transform.right * speed;
-	//}
+// 	//void Start(){
+// 		//rb.velocity = transform.right * speed;
+// 	//}
 
-	void OnTriggerEnter2D (Collider2D hitInfo)
-	{
-		Enemy enemy = hitInfo.GetComponent<Enemy>();
-		if (enemy != null)
-		{
-			enemy.TakeDamage(damage);
-		}
-		Destroy(gameObject);
-	}
-}
+// 	void OnTriggerEnter2D (Collider2D hitInfo)
+// 	{
+// 		//Enemy enemy = hitInfo.GetComponent<Enemy>();
+// 		if (enemy != null)
+// 		{
+// 			enemy.TakeDamage(damage);
+// 		}
+// 		Destroy(gameObject);
+// 	}
+// }
