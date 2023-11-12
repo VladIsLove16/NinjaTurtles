0686170bd520e482e5f759d36dffa99fcdaa72f8
Fluk <aleshagavr@mail.ru>
2023-11-13 01:39:42 +0300

    добавлен ёщё скрипт для нанесения урона персонажу

--- NinjaTurtles/Assets/Scripts/Bullet.cs
+++ NinjaTurtles/Assets/Scripts/Bullet.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour 
+{
 	
 	//public float speed = 20f;
 	public int damage = 40;
